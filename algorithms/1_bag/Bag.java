7bdc0a993aee99a9d51123a445e20c1e25bf22e5
Eric Turner <ericturnerdev@gmail.com>
2014-09-18 00:30:40 +0900

    Add bag skeleton only

---
+++ algorithms/1_bag/Bag.java
@@ -0,0 +1,19 @@
+public class Bag<Item> implements Iterable<Item>{
+
+  public Bag(){
+  }
+
+  public void add(Item item){
+  }
+
+  public boolean isEmpty(){
+  }
+
+  public int size(){
+  }
+
+  @Override
+  public Iterator iterator(){
+
+  }
+}
---
+++ algorithms/1_bag/Client.java
@@ -0,0 +1,21 @@
+public class Client
+{
+   public static void main(String[] args)
+   {
+      Bag<Double> numbers = new Bag<Double>();
+      for (int i=0; i<args.length; i++){
+        numbers.add(args[i]);
+      }
+      int N = numbers.size();
+      double sum = 0.0;
+      for (double x : numbers)
+         sum += x;
+      double mean = sum/N;
+      sum = 0.0;
+      for (double x : numbers)
+         sum += (x - mean)*(x - mean);
+      double std = Math.sqrt(sum/(N-1));
+      System.out.printf("Mean: %.2f\n", mean);
+      System.out.printf("Std dev: %.2f\n", std);
+  }
+}
