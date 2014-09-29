ca3e9e2285e9100277a03dab69227d64a878b45a
Eric Turner <ericturnerdev@gmail.com>
2014-09-30 08:52:28 +0900

    Get MergeSort kind of working (but still too slow)

--- algorithms/7_sort_list/Client.java
+++ algorithms/7_sort_list/Client.java
@@ -9,16 +9,37 @@ public class Client{
     node2.next = node3;
     node3.next = node4;
     System.out.println("Adding 5, 3, -7, 0.\nSize: " + MergeSort.getSize(node1));
+    System.out.println("Splitting the list");
+    ListNode node5 = MergeSort.splitNode(node1);
+    System.out.println("Right: " + printList(node5));
+    System.out.println("Left: " + printList(node1));
+
+    System.out.println("Testing the merge method by merging " + printList(node5) + " and " + printList(node1));
+    ListNode mergeResult = MergeSort.merge(node1, node5);
+    System.out.println("Merge result: " + printList(mergeResult));
+
+    node1 = new ListNode(5);
+    node2 = new ListNode(3);
+    node3 = new ListNode(-7);
+    node4 = new ListNode(0);
+    node1.next = node2;
+    node2.next = node3;
+    node3.next = node4;
+
+    System.out.println("Testing the sort  method with " + printList(node1));
+    ListNode sortResult = MergeSort.sortList(node1);
+    System.out.println("Sort result: " + printList(sortResult));
+
 
   }
 
   public static String printList(ListNode head){
     int size = MergeSort.getSize(head);
     String result = "[";
-    for(int i=0; i<size){
-      String += head.val + " ";
+    for(int i=0; i<size; i++){
+      result += head.val + ", ";
       head = head.next;
     }
-    return result += "[";
+    return result += "]";
   }
 }
--- algorithms/7_sort_list/MergeSort.java
+++ algorithms/7_sort_list/MergeSort.java
@@ -6,16 +6,18 @@ public class MergeSort{
   //I want it to work for a LinkedList, so I'm going to include the listNode class too.
 
 
-  public ListNode sort(ListNode head) {
+  public static ListNode sortList(ListNode head) {
+    if ( head == null || getSize(head) == 0){ return head; }
     //Base case: If it's only one element, just return that element
     if (head.next == null){ return head; }
     //Otherwise, cut it in half, sort the first half, sort the second half, and then merge them
     else{
       int size = getSize(head);
-      //Cut it in half
-      splitNode(head);
-      //Sort the halves
+      //Cut it in half and sort the halves
+      ListNode right = sortList(splitNode(head));
+      ListNode left = sortList(head);
       //Merge them
+      head = merge(left, right);
     }
     return head;
   }
@@ -26,20 +28,62 @@ public class MergeSort{
     return i;
   }
 
-  public static ListNode getNode(ListNode head, int index){
-    ListNode resultNode = head;
-    for(int i=0; i<index; i++){
-      resultNode = resultNode.next;
-    }
-  }
-
   //Returns head of right half
+  /*
+     Let's pretend we have 5 items: 3->4->1->2->5
+     Basically we want middlenode = 1, middleNodePre = 4.  halfSize = 5/2=2 so
+     //middleNode = 3, i=0: middleNode=4, middleNodePre = 4
+     //middleNode = 4, i=1: middleNode =1, middleNodePre = 4
+     //middleNode = 1, i=2:
+
+     //2 items: 3->4.  halfSize = 1.  middleNode=3, middleNodePre=3.
+     //middleNode=3, i=0: middleNode=4, middleNodePre still=3
+
+     4 items: 1->2->3->4, halfsize = 2
+     middleNode = 1, middleNodePre = 1
+     i=0, middleNode = 2.  middleNodePre = 2
+     i=1, middleNode = 3.  middleNodePre = 2.
+  */
   public static ListNode splitNode(ListNode head){
     int halfSize = getSize(head)/2;
     ListNode middleNode = head;
+    ListNode middleNodePre = head;
     for(int i=0; i<halfSize; i++){
       middleNode = middleNode.next;
+      if(i < halfSize-1){ middleNodePre = middleNode; }
     }
+    middleNodePre.next = null;
+    return middleNode;
+  }
+
+  public static ListNode merge(ListNode a, ListNode b){
+    //So we have a=[1, 3, 5] and b=[-2, -1, 7] and we want to merge them:
+    ListNode result;
+    if (a == null && b == null){ return null; }
+    else if (a == null) { result = new ListNode(b.val); b=b.next; }
+    else if (b == null) { result = new ListNode(a.val); a=a.next; }
+    else{
+      if (a.val < b.val){ result = new ListNode(a.val); a=a.next; }
+      else{ result = new ListNode(b.val); b=b.next; }
+    }
+
+    while(!(a == null && b == null)){
+      if (a == null) { add(result, b.val); b = b.next; }
+      else if (b == null) { add(result, a.val); a = a.next; }
+      else{
+        if (a.val < b.val){ add(result, a.val); a= a.next; }
+        else{ add(result, b.val); b = b.next; }
+      }
+    }
+    return result;
+  }
+
+  public static void add(ListNode a, int val){
+    ListNode currentNode = a;
+    while (currentNode.next != null){
+      currentNode = currentNode.next;
+    }
+    currentNode.next = new ListNode(val);
   }
 
 }
