557ba8e5822cc8590c3ef7c8c35d58ed0dce88d9
Eric Turner <ericturnerdev@gmail.com>
2015-02-15 00:07:27 +0900

    Put stack class inside Solution

--- algorithms/10_binary_tree_postorder/Solution.java
+++ algorithms/10_binary_tree_postorder/Solution.java
@@ -26,4 +26,29 @@ public class Solution {
       }
       return currentNode;
     }
+
+  public class Stack {
+    ArrayList<TreeNode> list;
+    public int size = 0;
+
+    public Stack() {
+      list = new ArrayList<TreeNode>();
+    }
+
+    public TreeNode pop() {
+      TreeNode retVal = list.get(list.size()-1);
+      list.remove(list.remove(list.size()-1));
+      size--;
+      return retVal;
+    }
+
+    public void push(TreeNode val) {
+      list.add(val);
+      size++;
+    }
+
+    public String toString() {
+      return list.toString();
+    }
+  }
 }
