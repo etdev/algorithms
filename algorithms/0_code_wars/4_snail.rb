deea5d4d2b936d316db37bc9be03f852b15ebd6b
Eric Turner <ericturnerdev@gmail.com>
2015-11-28 14:25:30 +0900

    Add code_wars snail

--- algorithms/0_code_wars/4_snail.rb
+++ algorithms/0_code_wars/4_snail.rb
@@ -1,4 +1,30 @@
 # http://www.codewars.com/kata/snail
-def snail(array)
-  
+# --- Iteration 1 (iterative) ---
+def snail(arr)
+  return arr.first if arr.first.length < 1
+  result = []
+  until arr.length == 0 do
+    current_length = arr.first.length
+    result << arr.delete_at(0)
+    arr = arr.transpose.reverse.each_slice(current_length).to_a.flatten(1)
+  end
+  result.flatten
 end
+
+# --- Iteration 2 (recursive) ---
+def snail(arr)
+  return arr.first if arr.length == 1
+  result = arr.delete_at(0)
+  arr = arr.transpose.reverse
+  return (result << snail(arr)).flatten
+end
+
+=begin NOTE
+* The recursive version can be written way more succintly as:
+def snail(array)
+  array.empty? ? [] : array.shift + snail(array.transpose.reverse)
+end
+* These solutions are all very slow, and all mutate the array, which they shouldn't.
+=end
+
+
