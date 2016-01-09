efc93e5719a2f9cec4cdc58d177b501a667e4e73
Eric Turner <ericturnerdev@gmail.com>
2016-01-09 14:08:59 +0900

    Add codewars 6 pile of cubes

--- 0_code_wars/6_pile_of_cubes.rb
+++ 0_code_wars/6_pile_of_cubes.rb
@@ -1,6 +1,5 @@
 # http://www.codewars.com/kata/5592e3bd57b64d00f3000047/
 # --- iteration 1 ---
-
 def find_nb(m)
   count = cube_sum = 1
   until cube_sum >= m
@@ -9,3 +8,17 @@ def find_nb(m)
   end
   cube_sum == m ? count : -1
 end
+
+# --- iteration 2 ---
+def find_nb(total_vol)
+  cube_count = 1
+  until total_vol <= 0
+    total_vol -= cube_count ** 3
+    cube_count += 1
+  end
+  total_vol == 0 ? cube_count-1 : -1
+end
+
+# sometimes it's good to use a passed in value and subtract from it when you're
+# concerned about whether it ends up being 0 etc, instead of instantiating a new
+# val like in iteration 1
