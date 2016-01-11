98c9973d381ba0c944fc0a07965835e3c460a588
Eric Turner <ericturnerdev@gmail.com>
2016-01-11 16:32:16 +0900

    Add code wars 6 lambdas for open closed

---
+++ 0_code_wars/6_lambdas_for_open_closed.rb
@@ -0,0 +1,17 @@
+# http://www.codewars.com/kata/53574972e727385ad10002ca/
+# --- iteration 1 ---
+spoken = ->(greeting) do
+  greeting << "."
+end
+
+shouted = ->(greeting) do
+  greeting.upcase << "!"
+end
+
+whispered = ->(greeting) do
+  greeting.downcase << "."
+end
+
+greet = ->(style, msg) do
+  style.call(msg)
+end
