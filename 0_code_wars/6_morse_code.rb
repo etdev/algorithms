e10f830aa567d0d4f9683df306f20b3e609ef571
Eric Turner <ericturnerdev@gmail.com>
2016-01-11 16:31:58 +0900

    Add code wars 6 morse code

---
+++ 0_code_wars/6_morse_code.rb
@@ -0,0 +1,19 @@
+# http://www.codewars.com/kata/54b724efac3d5402db00065e/
+# --- iteration 1 ---
+def decodeMorse(morse_code)
+  morse_code.strip!
+  morse_code.split(/\s/).map do |element|
+    MORSE_CODE[element]
+  end.join("|").gsub("|||", " ").gsub("|", "")
+end
+
+# I struggled with this one (clearly).  I couldn't find an elegant way to deal 
+# with ["S", " ", " ", "O", " ", " ", "S"] vs. ["S", "O", "S"],
+# which is why I used the "|" hack although I could've just used spaces.
+# I keep trying to do things like this:
+=begin
+  def decodeMorse(morse_code)
+    morse_code.scan(/[-\.]+/).map{ |x| MORSE_CODE[x] }.join(" ")
+  end
+=end
+# but it loses the locations of the original spaces.
