d3eea89a80daa59a88f0c2c56142f24468214fb7
Eric Turner <ericturnerdev@gmail.com>
2016-01-17 10:54:20 +0900

    Move importer files

--- 0_code_wars/0_importer/import.rb
+++ 0_code_wars/0_importer/import.rb
@@ -1,10 +1,12 @@
 require "open-uri"
 require "nokogiri"
+require "mechanize"
 require "pry"
 require "byebug"
 require_relative "./profile"
 require_relative "./challenge"
 
+binding.pry
 profile = Profile.new("etdev")
 challenge_links = profile.challenge_links
 challenge_links.each do |c_link|
