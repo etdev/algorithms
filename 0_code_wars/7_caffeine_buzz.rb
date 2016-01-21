7ebb89d9f59fc9e3e9c68daf11856adbef4c660a
Eric Turner <ericturnerdev@gmail.com>
2016-01-21 22:26:01 +0900

    Add code wars (7) caffeine buzz

---
+++ 0_code_wars/7_caffeine_buzz.rb
@@ -0,0 +1,19 @@
+# http://www.codewars.com/kata/5434283682b0fdb0420000e6/
+# iteration 1
+def caffeineBuzz(n)
+  if n % 3 == 0 && n % 4 == 0
+     str = "Coffee"
+  elsif n % 3 == 0
+    str = "Java"
+  end
+  return "mocha_missing!" unless str
+  n.even? ? str << "Script" : str
+end
+
+# iteration 2
+def caffeineBuzz(n)
+  str = "Coffee" if n % 12 == 0
+  str ||= "Java" if n % 3 == 0
+  return "mocha_missing!" unless str
+  n.even? ? str << "Script" : str
+end
