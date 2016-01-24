9ec3095389aa0960f9834860804a55094a5e77c1
Eric Turner <ericturnerdev@gmail.com>
2016-01-24 20:01:29 +0900

    Update importer to generate files

--- 0_code_wars/0_importer/challenge.rb
+++ 0_code_wars/0_importer/challenge.rb
@@ -15,8 +15,38 @@ class Challenge
   def fetch_solutions
     train_page = @mech.get(@url + TRAIN_PATH)
     train_page.css("script").reduce([]) do |acc, script_tag|
-      acc << script_tag.text if script_tag.text.start_with?("App.session")
+      if script_tag.text.start_with?("App.session")
+        script_tag.text =~ /(\"previousSolutions.*\"\]\}\]\})/
+        acc << JSON.parse("{#{$1}")
+      end
       acc
-    end.compact.first
+    end.compact.first["previousSolutions"]
+  rescue => e
+    puts "Failed to fetch solutions for #{@name}\n#{e.backtrace}"
+  end
+
+  def lang_parsed_solution_lists
+    @lang_parsed_solution_lists = solutions.map do |soln|
+      { url: @url,
+        name: @name,
+        soln_list: Hash[*soln.values_at("language", "solutions")]
+      }
+    end
+  end
+
+  def generate_output_files
+    lang_parsed_solution_lists.each do |soln|
+      soln[:soln_list].each do |lang, lang_solns|
+        file_name = "#{Dir.pwd}/output/#{soln[:name]}#{FILE_TYPES[lang]}"
+        File.open(file_name, File::WRONLY|File::CREAT|File::EXCL) do |soln_file|
+          soln_file.write("# #{@url}\n")
+          lang_solns.each.with_index do |lang_soln, i|
+            soln_file.write("# --- iteration #{i} --- ")
+            soln_file.write(lang_soln)
+            soln_file.write("\n\n")
+          end
+        end
+      end
+    end
   end
 end
--- 0_code_wars/0_importer/import.rb
+++ 0_code_wars/0_importer/import.rb
@@ -9,6 +9,8 @@ require_relative "./challenge"
 GH_OAUTH_SIGNIN_URL = "https://www.codewars.com/users/preauth/github/signin"
 USERNAME = ENV["GITHUB_USERNAME"]
 
+LANG_FILE_TYPE_MAPPINGS = { "Ruby" => ".rb" }
+
 def get_gh_authenticated_connection(username, password)
   mech = Mechanize.new
   gh_signed_in_page = mech.get(GH_OAUTH_SIGNIN_URL).form_with(name: nil) do |form|
@@ -19,11 +21,26 @@ def get_gh_authenticated_connection(username, password)
 end
 
 # authenticate
+print "\nAuthenticating..."
 mech = get_gh_authenticated_connection(ENV["GITHUB_USERNAME"], ENV["GITHUB_PW"])
+print "."
 profile = Profile.new(USERNAME, mech)
+print "."
 challenge_links = profile.challenge_links
+print "."
+print "\n"
+
+# fetch challenges
+print "Fetching challenges and solutions..."
 challenges = challenge_links.lazy.reduce([]) do |acc, c_link|
+  print "."
   acc << Challenge.new(mech, c_link)
-end.first
+end
+print "\n"
+
+# parse solutions
+solns = challenges.map(&:lang_parsed_solution_lists)
+challenges.map(&:generate_output_files)
+
 binding.pry
 x = 5
