f2a4424e393fb4c4b63d7f12430988bec90df23e
Eric Turner <ericturnerdev@gmail.com>
2016-02-11 12:15:31 +0900

    Add code wars (7) terminal game 2

---
+++ 0_code_wars/terminal_game_2.rb
@@ -0,0 +1,32 @@
+# http://www.codewars.com/kata/55e8beb4e8fc5b7697000036/
+# --- iteration 1 ---
+class Hero
+  def initialize
+    @position = [0, 0]
+  end
+
+  def move(dir)
+    case dir
+    when "up"
+      y_coord == 0 ? fail : @position[0] -= 1
+    when "down"
+      y_coord == 4 ? fail : @position[0] += 1
+    when "left"
+      x_coord == 0 ? fail : @position[1] -= 1
+    when "right"
+      x_coord == 4 ? fail : @position[1] += 1
+    end
+  end
+
+  def position
+    @position.join
+  end
+
+  def y_coord
+    @position[0]
+  end
+
+  def x_coord
+    @position[1]
+  end
+end
