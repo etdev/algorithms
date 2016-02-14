6c83f35cf85cc583ce530ba261f87019ded19f9c
Eric Turner <ericturnerdev@gmail.com>
2016-02-14 11:47:40 +0900

    Add character concatenation iteration 2

--- 0_code_wars/character_concatenation.rb
+++ 0_code_wars/character_concatenation.rb
@@ -8,3 +8,8 @@ def char_concat(word)
   left, right = word.slice!(0...word.size/2), word
   left.chars.zip(right.chars.reverse).zip(1..word_size_orig/2).join
 end
+
+# --- iteration 2 ---
+def char_concat(str)
+  (1..str.size/2).reduce(""){ |a, n| a << str[n-1] << str[-n] << n.to_s }
+end
