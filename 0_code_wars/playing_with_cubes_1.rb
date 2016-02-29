a95df8484c7f1818f43d75034ba2216e66e6e9de
Eric Turner <ericturnerdev@gmail.com>
2016-02-29 21:57:01 +0900

    Add Code wars (8) playing with cubes

---
+++ 0_code_wars/playing_with_cubes_1.rb
@@ -0,0 +1,11 @@
+# http://www.codewars.com/kata/55c0a79e20be94c91400014b/
+# --- iteration 1 ---
+class Cube
+  def set_side(side)
+    @side = side
+  end
+
+  def get_side
+    @side || 0
+  end
+end
