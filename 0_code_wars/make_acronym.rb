b6d532a30de5ecc78c65fe4ac7650e4ebfdc7110
Eric Turner <ericturnerdev@gmail.com>
2016-04-08 22:43:30 +0900

    Add code wars (7) - make acronym

---
+++ 0_code_wars/make_acronym.rb
@@ -0,0 +1,7 @@
+# http://www.codewars.com/kata/557efeb04effce569d000022/
+# --- iteration 1 ---
+def make_acronym(phrase)
+  return "Not a string" unless phrase.is_a?(String)
+  return "Not letters" unless phrase.downcase.delete("^a-z ").size == phrase.size
+  return phrase.split.map{ |word| word[0] }.join.upcase
+end
