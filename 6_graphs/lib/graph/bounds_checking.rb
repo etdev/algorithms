5a719f1c459969a89f23066588a27ebaecc7166d
Eric Turner <ericturnerdev@gmail.com>
2018-01-14 21:20:02 +0900

    Remove graphs

--- 6_graphs/lib/graph/bounds_checking.rb
+++
@@ -1,9 +0,0 @@
-module BoundsChecking
-  def with_bounds_check(*indices)
-    indices.each { |i| raise OutOfBoundsError if i >= vertex_count }
-
-    yield
-  end
-
-  class OutOfBoundsError < StandardError; end
-end
